a1536169599947f7cc6916240c665f4d14d0544e
Naidev7 <naidalyruiz@gmail.com>
2024-02-25 21:27:03 +0100

    files

---
+++ src/assets/data.js
@@ -0,0 +1,91 @@
+export const social = [
+  {
+    logo: "bx bxl-linkedin-square",
+    url: "https://www.linkedin.com/in/naidalyruiz/",
+  },
+  {
+    logo: "bx bxl-github",
+    url: "https://github.com/Naidev7",
+  },
+];
+export const info = [{ text: "About" }, { text: "Projects" }];
+export const technologies = [
+  {
+    text: "Javascript",
+    logo: "bx bxl-javascript",
+  },
+  {
+    text: "Sass",
+    logo: "bx bxl-sass",
+  },
+  {
+    text: "Git",
+    logo: "bx bxl-git",
+  },
+  {
+    text: "React",
+    logo: "bx bxl-react",
+  },
+  {
+    text: "Node.js",
+    logo: "bx bxl-nodejs",
+  },
+  {
+    text: "CSS",
+    logo: "bx bxl-css3",
+  },
+  {
+    text: "Tailwind",
+    logo: "bx bxl-tailwind-css",
+  },
+  {
+    text: "MongoDB",
+    logo: "bx bxl-mongodb",
+  },
+  {
+    text: "HTML5",
+    logo: "bx bxl-html5",
+  },
+  {
+    text: "Bootstrap",
+    logo: "bx bxl-bootstrap",
+  },
+  {
+    text: "MySQL",
+    logo: "bx bxl-data",
+  },
+  {
+    text: "Github",
+    logo: "bx bxl-github",
+  },
+  {
+    text: "Visual Studio",
+    logo: "bx bxl-visual-studio",
+  },
+];
+export const Projects = [
+  {
+    title: "Nakama",
+    description: "The project consists of creating a website that serves as a letter of introduction to our team of 5 people, which we have called Nakama. In terms of technologies, it has helped us to consolidate what we have been seeing in these weeks about layout: HTML, CSS, Flexbox, Media Queries, Github, Sass, Grid, etc. It has been an opportunity to start working as a team, get to know each other and apply the Agile philosophy",
+    image :'nakama.png',
+    color: '#1212334'
+  },
+  {
+    title: "Awesome Profile Cards",
+    description: "This project focuses on creating professional business cards using HTML, CSS and JavaScript. The goal is to provide users with a simple and effective tool to design professional business cards directly in the browser.",
+    image :'awesomeCards.png',
+    color: '#1212334'
+  },
+  {
+    title: "Hangman Game",
+    description: "The Hangman Game is a classic implementation of the famous word game. Use your JavaScript skills to guess the hidden word before the drawing of the hangman is completed.",
+    image :'hangman.png',
+    color: '#1212334'
+  },
+  {
+    title: "Rock, Paper, Scissors",
+    description: "In this game, you will face your own computer's artificial intelligence in an exciting battle of quick decisions. The mechanics are simple: each player chooses between rock, paper or scissors, and the outcome depends on the combination of choices.",
+    image :'rockPaperScissors.png',
+    color: '#1212334'
+  },
+];
