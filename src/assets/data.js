749388add900302a5a98cbab08e24c225541eaf3
Naidev7 <naidalyruiz@gmail.com>
2024-03-06 20:47:39 +0100

    cv btn

--- src/assets/data.js
+++ src/assets/data.js
@@ -8,6 +8,10 @@ export const social = [
     logo: "bx bxl-github",
     url: "https://github.com/Naidev7",
   },
+  {
+    logo:"bx bx-download",
+    url: "https://drive.google.com/file/d/1dXL4LjreG7z50kCBYmpQCEU_lqeb8uJe/view"
+  }
 ];
 export const info = [{ text: "About" }, { text: "Projects" }];
 export const technologies = [
@@ -70,25 +74,22 @@ export const projectsData = [
     title: "Nakama",
     description: "The project consists of creating a website that serves as a letter of introduction to our team of 5 people, which we have called Nakama. In terms of technologies, it has helped us to consolidate what we have been seeing in these weeks about layout: HTML, CSS, Flexbox, Media Queries, Github, Sass, Grid, etc. It has been an opportunity to start working as a team, get to know each other and apply the Agile philosophy",
     image :'image-nakama',
-    color: '#b3d39c'
+    color: '#b3d39c',
+    url: 'https://silviesc.github.io/project-promo-v-module-1-team-1/'
   },
   {
     title: "Awesome Profile Cards",
     description: "This project focuses on creating professional business cards using HTML, CSS and JavaScript. The goal is to provide users with a simple and effective tool to design professional business cards directly in the browser.",
     image :'image-awesomeCards',
-    color: '#428593'
+    color: '#428593',
+    url: 'https://naidev7.github.io/Awesome-profile-cards/'
   },
   {
-    title: "Hangman Game",
-    description: "The Hangman Game is a classic implementation of the famous word game. Use your JavaScript skills to guess the hidden word before the drawing of the hangman is completed.",
-    image :'image-hangman',
-    color: '#bd83ac'
-  },
-  {
-    title: "Rock, Paper, Scissors",
-    description: "In this game, you will face your own computer's artificial intelligence in an exciting battle of quick decisions. The mechanics are simple: each player chooses between rock, paper or scissors, and the outcome depends on the combination of choices.",
-    image :'image-rockPaperScissors',
-    color: '#008080'
+    title: "Digital Project Showcase",
+    description: "This project is a collaborative effort aimed at creating a digital storefront where users can view, create, and browse through a multitude of projects available on the web.",
+    image :'image-nakeyogami',
+    color: '#dad7cd',
+    url: 'https://project-promo-v-module-4-team-2.onrender.com/'
   },
 ];
 
@@ -97,7 +98,21 @@ export const buttons =  [
     github:'bx bxl-github' ,
     demo: 'bx bx-desktop',
     back: 'bx bxs-left-arrow-circle',
-    hrefgithub: 'https://github.com/Naidev7',
-    hrefdemo: ''
+    hrefgithub: 'https://github.com/Naidev7'
   }
 ]
+
+export const hrefdemo = [
+  {
+    id: 0,
+    url: 'https://silviesc.github.io/project-promo-v-module-1-team-1/'
+  },
+  {
+    id: 1,
+    url: 'https://naidev7.github.io/Awesome-profile-cards/'
+  },
+  {
+    id: 2,
+    url: 'https://project-promo-v-module-4-team-2.onrender.com/'
+  }
+] 
